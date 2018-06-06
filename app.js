3c454a3a24e9a8e4763cc5342c77efa21b63e226
LJ Skadi <LJ.Skadi@gmail.com>
2018-06-06 15:48:36 +0200

    Making it Cotton Candy 90s

--- app.js
+++ app.js
@@ -6,11 +6,11 @@ $(document).ready(function () {
     $('.submit-btn').hide();
     var clickCount = 0;
     var isSelected = false;
-    var answerRay = makeAnswer();
-    // console.log(answerRay);
-    var tempRay = $('.guess-pegs');
+    var solutionArray = pickACode();
+    // console.log(solutionArray);
+    var tempRay = $('.guess-history');
     var guessBoxArray = [];
-    var nextGrade = $($('.first-grade')[0]).parent()[0];
+    var nextFeedback = $($('.first-grade')[0]).parent()[0];
 
     for (var i = 9; i >= 0; i--) {
         guessBoxArray.push(tempRay[i]);
@@ -20,40 +20,40 @@ $(document).ready(function () {
     // and separates them by giving them a new attribute
     for (var i = 0; i < 10; i++) {
         var guessArray =
-            guessBoxArray[i].getElementsByClassName("guess-peg");
+            guessBoxArray[i].getElementsByClassName("guess-index");
         //console.log(guessArray);
         for (var j = 0; j < 4; j++) {
             $(guessArray[j]).attr('id', `g-${i}-${j}`);
         }
     }
 
-    // var nextGrade1 = $($('.first-grade')[0]).parent().prev();
-    // var nextGrade = $($('.first-grade')[0]).parent().prev()[0];
-    // var activeGrade =  nextGrade.getElementsByClassName("grad-pegs")[0];
-    // console.log(activeGrade);
+    // var nextFeedback1 = $($('.first-grade')[0]).parent().prev();
+    // var nextFeedback = $($('.first-grade')[0]).parent().prev()[0];
+    // var activeFeedback =  nextFeedback.getElementsByClassName("grad-pegs")[0];
+    // console.log(activeFeedback);
 
     var masterGuessArray = [[-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1],
-    [-1, -1, -1, -1]];
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1],
+                            [-1, -1, -1, -1]];
 
 
     // Inactivates the keypad for the acual guessing round
     $('.submit-btn').click(function () {
         $('.active').removeClass('active');
-        var gradeRay = getGrade();
-        console.log(gradeRay); //This array is evaluation of the guess
-        // console.log(answerRay); //This array is the solution code
-        var gradeBox = getGradeBox ();
-        placePegs(gradeRay, gradeBox);
-        //console.log(gradeBox);
+        var feedbackArray = getGrade();
+        console.log(feedbackArray); //This array is evaluation of the guess
+        // console.log(solutionArray); //This array is the solution code
+        var feedbackBox = getfeedbackBox ();
+        placePegs(feedbackArray, feedbackBox);
+        //console.log(feedbackBox);
         guess++;
         for (var i = 0; i < 4; i++) {
             $(`#g-${guess}-${i}`).addClass('active');
@@ -62,7 +62,7 @@ $(document).ready(function () {
     })
     //console.log(tempRay)
     //console.log(guessBoxArray)
-    // console.log($('.guess-pegs'));
+    // console.log($('.guess-history'));
 
 
     // Says that you mark your selected color, so that you know what you put in your guess
@@ -76,14 +76,14 @@ $(document).ready(function () {
     });
 
     // Organize that you can choose your color in the active guessing round
-    $('.guess-peg').click(function () {
+    $('.guess-index').click(function () {
         if (isSelected) {
             if ($(this).hasClass('active')) {
                 var number = parseInt($(this).css('border'));
                 if (number === 1) { //insert a digit
                     $(this).css('background', 'none');
                     $(this).css('background-color', selectedColor);
-                    $(this).css('border', '2px solid white');
+                    $(this).css('border', '2px solid #A8A8A8');
                     // Increases the number of clicks, so that we know, when to stop listening to the clicks
                     var coord = $(this).attr('id');
                     updateMasterArray(selectedColor, coord);
@@ -94,7 +94,7 @@ $(document).ready(function () {
                     }
                 } else { //digit removed
                     $(this).css('background', bGround);
-                    $(this).css('border', '1px solid white');
+                    $(this).css('border', '1px solid #787878');
                     // Decreases the number of clicks, when it is removed
                     //updateMasterArray(selectedColor, coord);
                     clickCount--;
@@ -103,7 +103,7 @@ $(document).ready(function () {
         }
     });
 
-    function makeAnswer() {
+    function pickACode() {
         var ray = [];
         for (var i = 0; i < 4; i++) {
             ray.push(Math.floor(Math.random() * 6) );
@@ -123,34 +123,34 @@ $(document).ready(function () {
     };
 
     function makeColorANumber(col) {
-        if (col === 'rgb(255, 0, 0)') return 0;
-        if (col === 'rgb(0, 128, 0)') return 1;
-        if (col === 'rgb(255, 255, 0)') return 2;
-        if (col === 'rgb(0, 0, 0)') return 3;
-        if (col === 'rgb(255, 255, 255)') return 4;
-        if (col === 'rgb(165, 42, 42)') return 5;
+        if (col === 'rgb(128, 147, 241)') return 0;
+        if (col === 'rgb(158, 120, 143)') return 1;
+        if (col === 'rgb(114, 221, 247)') return 2;
+        if (col === 'rgb(245, 156, 169)') return 3;
+        if (col === 'rgb(179, 136, 235)') return 4;
+        if (col === 'rgb(244, 244, 237)') return 5;
     }
 
     function getGrade() {
-        var gradRay = [];
+        var feedbackRay = [];
         // console.log(masterGuessArray[guess])
-        // console.log(answerRay);
+        // console.log(solutionArray);
         var aRay = [];
         for (var i = 0; i < 4; i++) {
             // console.log("I'm fillin up aRay");
-            aRay.push(answerRay[i]);
+            aRay.push(solutionArray[i]);
         }
         // Right Position Check
         for (var i = 0; i < 4; i++) {
             if (masterGuessArray[guess][i] === aRay[i]) {
-                gradRay.push('black-peg');    // 1 means that there is a color at the right position; GradRay can be pushed up to a length of NumberOf Digits length
+                feedbackRay.push('position');    // 1 means that there is a color at the right position; feedbackRay can be pushed up to a length of NumberOf Digits length
                 aRay[i] = -1;       // If the position is correct this will replace that color with -1 in the copy of the answer array, so that it cannot match with other colors
                 masterGuessArray[guess][i] = -2; // If the position of the guess is correct, it will replace that number in the guess array with a -2 so that it cannot match with another color anymore
             }
             //console.log("This is aRay: " + aRay);
             //console.log("This is masterGuessArray[guess]: " + masterGuessArray[guess]);
-            //console.log("This is answerRay: " + answerRay);
-            //console.log("This is gradRay: " + gradRay);
+            //console.log("This is solutionArray: " + solutionArray);
+            //console.log("This is feedbackRay: " + feedbackRay);
         }
 
 
@@ -158,38 +158,38 @@ $(document).ready(function () {
         for (var i = 0; i < 4; i++) {
             for (var j = 0; j < 4; j++) {
                 if (masterGuessArray[guess][i] === aRay[j]) {
-                    gradRay.push('white-peg');   // Means that, if a color is correct, that is displayed by 2 in the GradArray
+                    feedbackRay.push('color');   // Means that, if a color is correct, that is displayed by 2 in the GradArray
                     aRay[j] = -1;
                     masterGuessArray[guess][i] = -2;
                 }
             }
         }
-        return gradRay;
+        return feedbackRay;
     }
 
 
-    //var nextGrade = $($('.first-grade')[0]).parent().prev()[0];
-    //console.log(activeGrade);
+    //var nextFeedback = $($('.first-grade')[0]).parent().prev()[0];
+    //console.log(activeFeedback);
     
-    function getGradeBox(){ // This function traverse through the GradeBox
-    // var nextGrade1 = $($('.first-grade')[0]).parent().prev();
+    function getfeedbackBox(){ // This function traverse through the feedbackBox
+    // var nextFeedback1 = $($('.first-grade')[0]).parent().prev();
     //var firstGrade = $($('.first-grade')[0]).parent().prev()[0];
-   // console.log(nextGrade);
-    var activeGrade =  nextGrade.getElementsByClassName("grade-pegs")[0];
-    //console.log(activeGrade);
-    nextGrade = $(nextGrade).prev()[0];
-    // console.log(activeGrade);
-    return activeGrade;
+   // console.log(nextFeedback);
+    var activeFeedback =  nextFeedback.getElementsByClassName("feedback-history")[0];
+    //console.log(activeFeedback);
+    nextFeedback = $(nextFeedback).prev()[0];
+    // console.log(activeFeedback);
+    return activeFeedback;
     }
 
     function placePegs(ray, box){
-        var pegRay = box.getElementsByClassName("grade-peg");
+        var pegRay = box.getElementsByClassName("feedback-index");
         console.log(pegRay);
         for (var i = 0; i < ray.length; i++){
             // Take an element of the pegRay and add a class
             $(pegRay[i]).addClass(`${ray[i]}`);
         }
-        $('.white-peg').css('background-color', 'none').css('background-color', 'green');
-        $('.black-peg').css('background-color', 'none').css('background-color', 'red');
+        $('.position').css('background-color', 'none').css('background-color', '#00ccff');
+        $('.color').css('background-color', 'none').css('background-color', '#ff66ff');
     }
 });
